d21359d9184496eadd4544f47591ea64d3845743
Dani Kline <danikline@Danis-MacBook-Pro.local>
2022-08-16 14:04:27 -0400

    Added codePRCheck workflow

---
+++ .github/scripts/codeprcheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Requirements:
+# - Sarif file from Snyk Code scan
+# - Workflow in directory the Snyk Code Scan & diff are run on 
+# - Update variable MERGE_BASE to merge-base (or otherwise update diff as needed eg. between PR & destination branch)
+# - Node-JQ installed 
+MERGE_BASE=main
+# when tested with the juliet-test-suite-java repo, diff only saw tracked files so did not give added files, only modified
+# I think when I first tested it in a small local repo I had to add/track new files
+# could be me misunderstanding how to execute this or the only way to achieve is something like:
+# https://stackoverflow.com/questions/855767/can-i-use-git-diff-on-untracked-files
+# this outputs the diff to a jq friendly way and saves it to a variable
+FILES_IN_DIFF_LIST=$(git diff --merge-base $MERGE_BASE --diff-filter=AM --name-only | jq --raw-input .| jq --slurp .)
+# select results where the location uri does not match a filename from the diff
+# delete results that were selected
+# output into a file
+jq --argjson FILES_IN_DIFF_LIST "$FILES_IN_DIFF_LIST" 'del(.runs[0].results[] | select([.locations[].physicalLocation.artifactLocation.uri] | inside ($FILES_IN_DIFF_LIST) | not))' sarif > updatedsarif.json
