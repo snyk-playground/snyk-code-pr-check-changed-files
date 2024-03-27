ff1f8da32f681c013d160b4923a36aaa9c9ed6a7
Dani Does Stuff <88793362+DaniKline@users.noreply.github.com>
2024-03-27 11:40:28 -0400

    Update typeorm-db.js

--- typeorm-db.js
+++ typeorm-db.js
@@ -44,3 +44,21 @@ typeorm.createConnection({
   console.error('failed connecting and seeding users to the MySQL database')
   console.error(err)
 })
+
+// Edited to add a vulnerability from routes/index.js for an example of a Snyk Code PR Check 
+exports.loginHandler = function (req, res, next) {
+  if (validator.isEmail(req.body.username)) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+  } else {
+    return res.status(401).send()
+  }
+};
