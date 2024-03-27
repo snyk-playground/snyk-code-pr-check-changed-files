3cda2b058c71a21c89f4b0f35069bc78215ae98b
Dani Does Stuff <88793362+DaniKline@users.noreply.github.com>
2024-03-27 11:42:20 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,23 @@ exports.index = function (req, res, next) {
     });
 };
 
+// Edited to add a vulnerability from routes/index.js for an example of a Snyk Code PR Check 
+exports.loginHandler = function (req, res, next) {
+  if (validator.isEmail(req.body.username)) {
+    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
+      if (users.length > 0) {
+        const redirectPage = req.body.redirectPage
+        const session = req.session
+        const username = req.body.username
+        return adminLoginSuccess(redirectPage, session, username, res)
+      } else {
+        return res.status(401).send()
+      }
+    });
+  } else {
+    return res.status(401).send()
+  }
+};
 exports.loginHandler = function (req, res, next) {
   if (validator.isEmail(req.body.username)) {
     User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
